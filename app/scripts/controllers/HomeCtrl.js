6f0f36ba130e9442464418f2c217122e7bd64361
Chris Boatwright <christopher.boatwright@gmail.com>
2017-08-05 18:02:02 -0400

    Commit 03

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,8 +1,7 @@
 (function() {
     function HomeCtrl(Room) {
         /*References Room function which lives inside of Room.js. That function creates an empty Room object, then gets all the children in rooms array, then sets that as the variable rooms, then sets that equal to the method Room.all*/
-        this.array1 = [1, 2, 3, ];
-        this.tempTitle = "Ermahgerd!";
+        this.title = "Welcome to Bloc Chat!!!";
         this.room = Room.all //method created in Room.js to list all chat rooms
     }
 
