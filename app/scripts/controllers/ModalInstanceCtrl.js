29935cc4141ad81f4111c99538e066b3cf59c9d9
Chris Boatwright <christopher.boatwright@gmail.com>
2017-08-10 18:53:08 -0400

    Commit 2

---
+++ app/scripts/controllers/ModalInstanceCtrl.js
@@ -0,0 +1,18 @@
+(function() {
+    /*
+        this controller listens for instructions from inside our modal,
+        similar to how ModalCtrl listens to instructions from outside
+        of it. when the cancel button is clicked inside of modal.html,
+        this.cancel() is executed, which calls the $uibModalInstance 
+        service and dismisses the modal. 
+    */
+    function ModalInstanceCtrl($uibModalInstance) {
+        this.cancel = function() {
+            $uibModalInstance.dismiss('dismiss')
+        }
+    }
+
+    angular
+        .module('angularModals')
+        .controller('ModalInstanceCtrl', ['$uibModalInstance', ModalInstanceCtrl])
+})()
