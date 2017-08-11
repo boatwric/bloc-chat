11f503fe4a9f1c8c3e8ab56c1afc68c846b468ad
Chris Boatwright <christopher.boatwright@gmail.com>
2017-08-10 20:29:52 -0400

    Commit 03

--- app/scripts/controllers/ModalCtrl.js
+++ app/scripts/controllers/ModalCtrl.js
@@ -1,11 +1,19 @@
 (function() {
-        function ModalCtrl(function($uibModal, $log, Room) { //taken straight from documentation, only removed $document because I'm not applying it to the whole page
-                /*References Room function which lives inside of Room.js. That function creates an empty Room object, then gets all the children in rooms array, then sets that as the variable rooms, then sets that equal to the method Room.add*/
-                this.title = "Create New Room!!!";
-                this.add = Room.add //method created in Room.js to create new chat room
-            }
+    function ModalCtrl($uibModal, $log, Room) { //taken straight from documentation, only removed $document because I'm not applying it to the whole page
+        /*References Room function which lives inside of Room.js. That function creates an empty Room object, then gets all the children in rooms array, then sets that as the variable rooms, then sets that equal to the method Room.add*/
+        this.title = "Create New Room!!!";
 
-            angular
-            .module('blocChat')
-            .controller('HomeCtrl', ['Room', HomeCtrl]);
-        })();
+        this.open = function() {
+            var modalInstance = $uibModal.open({ //copied directly from documentation
+                animation: $ctrl.animationsEnabled,
+                templateUrl: 'templates/modal.html',
+                controller: 'ModalInstanceCtrl',
+                controllerAs: 'modal'
+            });
+        };
+    };
+
+    angular
+        .module('blocChat')
+        .controller('ModalCtrl', ['$uibModal', ModalCtrl])
+})();
--- app/scripts/controllers/ModalInstanceCtrl.js
+++ app/scripts/controllers/ModalInstanceCtrl.js
@@ -1,18 +1,19 @@
 (function() {
-    /*
-        this controller listens for instructions from inside our modal,
-        similar to how ModalCtrl listens to instructions from outside
-        of it. when the cancel button is clicked inside of modal.html,
-        this.cancel() is executed, which calls the $uibModalInstance 
-        service and dismisses the modal. 
-    */
     function ModalInstanceCtrl($uibModalInstance) {
         this.cancel = function() {
             $uibModalInstance.dismiss('dismiss')
-        }
+        };
     }
 
     angular
-        .module('angularModals')
+        .module('blocChat')
         .controller('ModalInstanceCtrl', ['$uibModalInstance', ModalInstanceCtrl])
 })()
+
+/*
+this controller listens for instructions from inside our modal,
+similar to how ModalCtrl listens to instructions from outside
+of it. when the cancel button is clicked inside of modal.html,
+this.cancel() is executed, which calls the $uibModalInstance 
+service and dismisses the modal. 
+*/
