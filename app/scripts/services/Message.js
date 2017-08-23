756049157dddf2717bd9b5cbc3b570201231ddab
Chris Boatwright <christopher.boatwright@gmail.com>
2017-08-22 22:15:13 -0400

    Commit 01

---
+++ app/scripts/services/Message.js
@@ -0,0 +1,23 @@
+/*
+Look familiar? Pretty much identical setup to Room.js. $firebaseArray is passed to the function, and inside there's an empty object, a path to the database, and then passing that path off to your $firebaseArray. Then you call your object and assign it whatever methods you want to populate it. Down at the botton, you've got a message factory that pulls in the $firbaseArray and the service itself as dependencies. Piece of cake, right?
+*/
+
+
+(function() {
+        function Message($firebaseArray) {
+            var Message = {};
+            var ref = firebase.database().ref().child("messages");
+            var messages = $firebaseArray(ref);
+
+            Message.getByRoomId = function(roomId) {
+                // Filter the messages by their room ID.
+            };
+
+            return Message;
+        };
+    }
+
+    angular
+    .module('blocChat')
+    .factory('Message', ['$firebaseArray', Message]);
+})();
