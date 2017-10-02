6fef10f7a113b7fdf1b1cb9df6e81acc8405f9b9
Chris Boatwright <christopher.boatwright@gmail.com>
2017-10-02 15:33:10 -0400

    Commit 6.01

--- app/scripts/services/Message.js
+++ app/scripts/services/Message.js
@@ -4,7 +4,7 @@ Look familiar? Pretty much identical setup to Room.js. $firebaseArray is passed 
 
 
 (function() {
-    function Message($firebaseArray) {
+    function Message($firebaseArray, $cookies) {
         var Message = {};
         var ref = firebase.database().ref().child("messages");
         var messages = $firebaseArray(ref);
@@ -15,10 +15,24 @@ Look familiar? Pretty much identical setup to Room.js. $firebaseArray is passed 
             //now that I have $id, can pass into this method to get associated messages. 
         };
 
+        Message.send = function(newMessage) {
+            var date = new Date(); //need this to filter in sentAt
+            var msg = {
+                username: $cookies.get('blocChatCurrentUser'), //taken from app.js, line 18
+                content: newMessage, //entering content is what gets the whole thing rolling
+                sentAt: {
+                    { date | date: 'medium' }
+                }, //needs new Date, then filtered, taken from Angular DatePipe documentation
+                roomId: room.$id //taken from HomeCtrl
+            };
+            messages.$add(msg); //put new message object into $firebaseArray 
+            msg = ''; //resets to take in new message once old one is sent off (???)
+        };
+
         return Message;
     };
 
     angular
         .module('blocChat')
-        .factory('Message', ['$firebaseArray', Message]);
+        .factory('Message', ['$firebaseArray', '$cookies', Message]);
 })();
